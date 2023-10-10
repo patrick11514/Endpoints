2c2446171523dc381494adf735508c1af4ed6a84
patrick11514 <ja@patrick115.eu>
2023-10-10 17:27:04 +0200

    Added index file

---
+++ index.d.ts
@@ -0,0 +1,38 @@
+import * as z from 'zod'
+
+export type EndpointMethod = 'GET' | 'POST' | 'PUT' | 'DELETE'
+
+export type ErrorSchema = {
+    status: false
+    error: string
+}
+
+export declare class Endpoint<T> {
+    constructor(endpoint: string, method: EndpointMethod)
+    fetch(): Promise<T | ErrorSchema>
+    fetchSafe(): Promise<
+        | {
+              status: false
+              error: z.ZodError<T>
+          }
+        | {
+              status: false
+              error: object
+          }
+        | {
+              status: false
+              error: unknown
+              errorData: string
+          }
+        | {
+              status: true
+              errorSchema: true
+              data: ErrorSchema
+          }
+        | {
+              status: true
+              errorSchema: false
+              data: T
+          }
+    >
+}
