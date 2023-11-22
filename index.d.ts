7f6ba5ab606f123a53d90fe1ba962878350cbc38
Patrik Mintěl <ja@patrick115.eu>
2023-11-22 22:58:23 +0100

    ....

--- index.d.ts
+++ index.d.ts
@@ -10,14 +10,21 @@ type ErrorSchema = {
 
 export declare const defaultErrorSchema: z.ZodType<ErrorSchema>
 export declare class Endpoint<T, I, E> {
-    constructor(
-        endpoint: string,
-        method: EndpointMethod,
-        schema: z.ZodType<T>,
-        headers?: HeadersInit,
-        inputSchema?: z.ZodType<I>,
-        errorSchema?: z.ZodType<E>,
-    )
+    constructor({
+        endpoint,
+        method,
+        schema,
+        headers,
+        inputSchema,
+        errorSchema,
+    }: {
+        endpoint: string
+        method: EndpointMethod
+        schema: z.ZodType<T>
+        headers?: HeadersInit
+        inputSchema?: z.ZodType<I>
+        errorSchema: z.ZodType<E>
+    })
     fetch(data?: I): Promise<T | E | ErrorSchema>
     fetchSafe(data?: I): Promise<
         | {
